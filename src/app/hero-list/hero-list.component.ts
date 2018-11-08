f0b0f823de8cea8934e5b3bc6dd81ef969d5d88c
QiGe <gridwang@gmail.com>
2018-11-08 10:05:07 +0800

    first

---
+++ src/app/hero-list/hero-list.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-hero-list',
+  templateUrl: './hero-list.component.html',
+  styleUrls: ['./hero-list.component.css']
+})
+export class HeroListComponent implements OnInit {
+  hero = '华盛顿';
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
